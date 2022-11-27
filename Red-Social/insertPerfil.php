7abb9dca8ad9e12c71afc24f85d9a00da165ab01
ThePugo <95140143+ThePugo@users.noreply.github.com>
2022-11-27 20:56:48 +0100

    Add files via upload

---
+++ Red-Social/insertPerfil.php
@@ -0,0 +1,21 @@
+<html>
+    <!-- INSERTA EN LA BASE DE DATOS (CON SQL) LOS DATOS QUE ENCUENTRA EN EL FORM-->
+    <head>
+        <meta http-equiv="refresh" content="2;url=principal.php" />
+    </head>
+    <?php
+        $nombre = $_POST["nombre"];
+        $apellidos = $_POST["apellidos"];
+        $genero= $_POST["genero"];
+        $edad= $_POST["edad"];
+        $nombreUsuario=$_POST["nombreUsuario"];
+        $string="INSERT into perfil set nombre = \"$nombre\", apellidos = \"$apellidos\", genero = \"$genero\", edad = \"$edad\", nombreUsuario = \"$nombreUsuario\"";
+        if ($edad=="" || $nombreUsuario=="") {
+            echo "FALTAN DATOS MI COMPADRE METELOS BIEN DUROS AHI";
+        } else {
+            echo "Creando perfil...";
+            include "conexiones.php";
+            $insert=mysqli_query($conexio,$string);
+        }
+    ?>
+</html>
