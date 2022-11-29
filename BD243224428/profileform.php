daf4e3bbe0179f9775ac570e1a993635845989ca
ThePugo <95140143+ThePugo@users.noreply.github.com>
2022-11-29 17:47:24 +0100

    Add files via upload

---
+++ BD243224428/profileform.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Crea tu perfil</title>
+        <link rel="stylesheet" type="text/css" href="profilestyle.css">
+    </head>
+
+    <body>
+        
+        <div class="profilebox">
+            <div class="center">
+                <form method="post" action="insertPerfil.php">
+
+                    <center>
+                        <h2>CREA TU PERFIL</h2><br>
+                    </center>
+
+                    <label>Nombre de Usuario</label>
+                    <input type="text" value="", name="nombreUsuario" placeholder="Introduzca su nombre de usuario" class="roundborder" required maxlength="20">
+
+                    <label>Password</label>
+                    <input type="password" value="", name="password" placeholder="Introduzca su password" class="roundborder" required maxlength="20">
+
+                    <label>Nombre</label>
+                    <input type="text" value="", name="nombre" placeholder="Introduzca su nombre" class="roundborder" required maxlength="15">
+
+                    <label>Apellidos</label> 
+                    <input type="text" value="", name="apellidos"  placeholder="Introduzca sus apellidos (opcional)" class="roundborder" maxlength="25">
+
+                    <label>Genero</label>
+                    <select name="genero">
+                        <option value="Hombre">Hombre</option>
+                        <option value="Mujer">Mujer</option>
+                        <option value=NULL>Prefiero no especificar</option>
+                    </select>
+
+                    <br><br><label>Edad</label>
+                    <input type="number" value="", name="edad"  placeholder="Introduzca su edad" class="roundborder" required="required">
+
+                    <label>Biografia</label>
+                    <input type="text" value="", name="biografia" placeholder="Introduzca su biografía (opcional)" class="roundborder" maxlength="150">
+
+                    <input type="submit" value="Crear">
+                    <input type="submit" value="Volver Atras" onclick="location='../BD2X7682807/login.html'" />
+                </form>
+            </div>      
+        </div>
+    
+    </body>
+</html>
