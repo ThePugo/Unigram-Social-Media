8941461ded85f231b56b85fefd834bd49d5d384b
Arya Vert <aryaigusti02@gmail.com>
2022-12-01 18:53:43 +0100

    updated

--- BD243475994/mensajes.php
+++ BD243475994/mensajes.php
@@ -4,23 +4,36 @@
         <title>Mensajes</title>
         <link rel="stylesheet" type="text/css" href="mensajesstyle.css">
     </head>
-<div class = "usersbox">
-    <?php
-        session_start();
-        $nombreusuario = $_SESSION['nombreUsuario'];
-        include "../conexiones.php";
-        $stringPublicacion="SELECT nombreUsuario FROM usuario ORDER BY nombreusuario ASC";
-        $nombresusuarios=mysqli_query($conexio,$stringPublicacion);
-        while($reg=mysqli_fetch_array($nombresusuarios)){
-            if($reg["nombreUsuario"]!=$nombreusuario){?>
-                <button id="userbox">
-                    <a href="verMensajes.php" class="linksmessage"><?php echo $reg["nombreUsuario"] ?></a>
-                </button>
+    
+    <body>
+        <div class = "usersbox" style="overflow-y:auto;">
+            <div class = "messagetitle"><h1>Mis mensajes</h1>
+                <a href="../principal.php">
+                    <button id="homebutton">
+                    </button>
+                </a>
+            </div>
             <?php
-            }
-        }
-    ?>
-</div>
+                session_start();
+                $nombreusuario = $_SESSION['nombreUsuario'];
+                include "../conexiones.php";
+                $stringPublicacion="SELECT nombreUsuario FROM usuario ORDER BY nombreusuario ASC";
+                $nombresusuarios=mysqli_query($conexio,$stringPublicacion);
+                while($reg=mysqli_fetch_array($nombresusuarios)){
+                    if($reg["nombreUsuario"]!=$nombreusuario){?>
+                        <button id="userbox">
+                            <a href="verMensajes.php" class="linksmessage"><?php echo $reg["nombreUsuario"] ?></a>
+                        </button>
+                    <?php
+                    }
+                }
+            ?>
+        </div>
+        <div class="insertmessagebox">
+            <textarea placeholder="Inserte un mensaje"></textarea>
+        </div>
+    </body>    
+
 </html>
 
 
