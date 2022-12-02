ccb28ad041c8de63d29055ac994b188e81689b8d
Arya Vert <aryaigusti02@gmail.com>
2022-12-02 13:43:40 +0100

    update

--- BD243475994/verMensajes.php
+++ BD243475994/verMensajes.php
@@ -1,14 +1,25 @@
-<?php
-    include "mensajes.php";
-    $receptor=$_GET['receptor'];
-    ?>
-    <div class="insertmessagebox">
-    <form method="post" action="enviarMensaje.php">
-        <textarea  name = "mensaje" required maxlength="180" placeholder="Inserte un mensaje"></textarea>
-        <input type="submit" value="Enviar" class="roundborder">
-    </form>   
-    </div>
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            include "mensajes.php";
+            $receptor=$_GET['receptor'];
+        ?>
+        <div class = "chatmessagesbox">
+            <div class = "chattitle">
+                <center>
+                    <h1>Chat con "Nombre de usuario"</h1>
+                    <hr>
+                </center>
+            </div>
+        </div>
+        <div class="insertmessagebox">
+            <form method="post" action="enviarMensaje.php">
+                <textarea  name = "mensaje" required maxlength="180" placeholder="Inserte un mensaje"></textarea>
+                <input type="image" src="send.png" alt="Submit">
+            </form>   
+        </div>
 
-    <?php
+    </body>      
 
-?>
+</html>
