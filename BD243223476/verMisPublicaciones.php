415e78cd22a60f27cf9976113b3d9a02c9697804
ThePugo <95140143+ThePugo@users.noreply.github.com>
2022-12-08 18:26:29 +0100

    Add files via upload

---
+++ BD243223476/verMisPublicaciones.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title>Mis publicaciones</title>
+        <link rel="stylesheet" type="text/css" href="publicacionesstyle.css">
+    </head>
+<?php
+    include "../conexiones.php";
+    session_start();
+    $nombreUsuario=$_SESSION['nombreUsuario'];
+    $query="SELECT * FROM publicacion WHERE nombreUsuario='$nombreUsuario'";
+    $consulta=mysqli_query($conexio,$query);?>
+    <button onclick="location.href = '../principal.php';" style="position:relative;border:none;background-color:lightsteelblue;"><img src="../BD243224428/home.png" height="50px"></img></button>
+    <center><h1>Mis publicaciones</h1></center>
+    <?php
+    while($reg=mysqli_fetch_array($consulta)) {
+        ?>
+        <div class="post">
+            <?php
+                $nusuario=$reg["nombreUsuario"];
+                #ENLACE A PERFIL?>
+                <a href="../BD243224428/verPerfil.php?nombredeusuario=<?php echo $nusuario ?>" class="linksposts"><?php echo $nusuario ?></a><br><br>
+                <?php echo $reg["descripcion"];?>
+                <div class="date">
+                    <?php echo $reg["fechaCreacion"];?>
+                </div>
+        </div>
+        <?php
+            }
+    ?>
+</html>
