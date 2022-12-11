13795f5c1c6731bc94b2fe42d6ca9c320d18df2e
ThePugo <95140143+ThePugo@users.noreply.github.com>
2022-12-11 21:37:00 +0100

    Update deletePublicacion.php

--- BD243223476/deletePublicacion.php
+++ BD243223476/deletePublicacion.php
@@ -1,11 +1,25 @@
 <html>
     <?php
         $idPublicacion=$_GET['id'];
-        $string="DELETE from publicacion WHERE idPublicacion='$idPublicacion'";
         include "../conexiones.php";
-        $insert=mysqli_query($conexio,$string);
+        $consulta="SELECT * FROM publicacion WHERE idPublicacion2='$idPublicacion'";
+        $c=mysqli_query($conexio,$consulta);
+        if (mysqli_num_rows($c)==0) {
+            $string="DELETE from publicacion WHERE idPublicacion='$idPublicacion'";
+            $insert=mysqli_query($conexio,$string);
+            ?>
+            <head>
+                <meta http-equiv="refresh" content="0;url=../principal.php" />
+            </head>
+            <?php
+        }
+        else {
+            echo "NO SE PUEDE BORRAR UNA PUBLICACIÓN QUE TIENE REENVÍOS";
+            ?>
+            <head>
+                <meta http-equiv="refresh" content="2;url=../principal.php" />
+            </head>
+            <?php
+        }
     ?>
-    <head>
-        <meta http-equiv="refresh" content="0;url=../principal.php" />
-    </head>
 </html>
